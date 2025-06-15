a5e9be4815386ccd5a4a7829caa43799fb1c50b2
wuyong <biggodw@gmail.com>
2025-06-15 14:49:30 +0800

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+const serverless = require('serverless-http');
+
+const prisma = new PrismaClient()
+const app = express()
+app.get('/',async(req,res)=>{
+    res.send('hello express')
+})
+
+
+app.get('/user',async(req,res)=>{
+    const user = await prisma.user.findMany()
+    console.log(user)
+    res.send(user)
+})
+app.post("/user/add",async(req,res)=>{
+    const {name,email} = req.body
+    const result = await prisma.user.create({
+        data:{
+            name:name,
+            email:email
+        }
+    })
+    res.send(result)
+})
+
+app.listen('3003',()=>{
+    console.log('running')
+})
+
+module.exports = app; // 直接导出 app（Vercel 自动处理）
+module.exports.handler = serverless(app); // 备用方案
